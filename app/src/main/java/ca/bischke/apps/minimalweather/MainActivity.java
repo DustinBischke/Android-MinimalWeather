cb039f13d0f222640c2c0dfeb005cefcc13370db
Dustin <dustin@bischke.ca>
2019-06-09 22:56:10 -0600

    Added Weather Conditions

--- app/src/main/java/ca/bischke/apps/minimalweather/MainActivity.java
+++ app/src/main/java/ca/bischke/apps/minimalweather/MainActivity.java
@@ -3,7 +3,7 @@ package ca.bischke.apps.minimalweather;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -34,8 +34,8 @@ public class MainActivity extends AppCompatActivity
 
         setDateTime();
 
-        String city = "Carlow";
-        String countryCode = "IE";
+        String city = "Edmonton";
+        String countryCode = "CA";
 
         JsonTask jsonTask = new JsonTask();
         jsonTask.execute(url + city + "," + countryCode + "&appid=" + apiKey);
@@ -64,6 +64,12 @@ public class MainActivity extends AppCompatActivity
         textTime.setText(time);
     }
 
+    private String getCountryFromCode(String countryCode)
+    {
+        Locale locale = new Locale("", countryCode);
+        return locale.getDisplayCountry();
+    }
+
     private int kelvinToCelsius(double kelvin)
     {
         double celsius = kelvin - 273.15;
@@ -74,17 +80,63 @@ public class MainActivity extends AppCompatActivity
     {
         if (temperature < 10)
         {
-            double windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+            double windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16)
+                    + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
             return (int) Math.round(windChill);
         }
 
         return temperature;
     }
 
-    private String getCountryFromCode(String countryCode)
+    private void setCondition(int condition)
     {
-        Locale locale = new Locale("", countryCode);
-        return locale.getDisplayCountry();
+        TextView textCondition = findViewById(R.id.textCondition);
+        ImageView imageIcon = findViewById(R.id.imageIcon);
+
+        if (condition >= 200 && condition < 300)
+        {
+            textCondition.setText(R.string.thunderstorm);
+            imageIcon.setImageResource(R.drawable.ic_thunderstorm);
+        }
+        else if (condition >= 300 && condition < 400)
+        {
+            textCondition.setText(R.string.drizzle);
+            imageIcon.setImageResource(R.drawable.ic_drizzle);
+        }
+        else if (condition >= 500 && condition < 600)
+        {
+            textCondition.setText(R.string.rain);
+            imageIcon.setImageResource(R.drawable.ic_rain);
+        }
+        else if (condition >= 600 && condition < 700)
+        {
+            textCondition.setText(R.string.snow);
+            imageIcon.setImageResource(R.drawable.ic_snow);
+        }
+        else if (condition >= 700 && condition < 800)
+        {
+            // TODO
+        }
+        else if (condition == 800)
+        {
+            textCondition.setText(R.string.clear);
+            imageIcon.setImageResource(R.drawable.ic_clear);
+        }
+        else if (condition == 801 || condition == 802)
+        {
+            textCondition.setText(R.string.partly_cloudy);
+            imageIcon.setImageResource(R.drawable.ic_partly_cloudy);
+        }
+        else if (condition == 803)
+        {
+            textCondition.setText(R.string.mostly_cloudy);
+            imageIcon.setImageResource(R.drawable.ic_mostly_cloudy);
+        }
+        else if (condition == 804)
+        {
+            textCondition.setText(R.string.cloudy);
+            imageIcon.setImageResource(R.drawable.ic_cloudy);
+        }
     }
 
     private class JsonTask extends AsyncTask<String, Void, String>
@@ -139,9 +191,7 @@ public class MainActivity extends AppCompatActivity
                 for (int i = 0; i < jsonArray.length(); i++)
                 {
                     JSONObject jsonPart = jsonArray.getJSONObject(i);
-
-                    TextView textCondition = findViewById(R.id.textCondition);
-                    textCondition.setText(jsonPart.getString("main"));
+                    setCondition(jsonPart.getInt("id"));
                 }
 
                 JSONObject main = jsonObject.getJSONObject("main");
